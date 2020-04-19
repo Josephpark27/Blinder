38bdec0ad65ea3b53a1d3b37fc7888a00e8a23a9
Joseph Park <josephpark534@berkeley.edu>
2020-04-18 21:12:02 -0700

    add files

---
+++ app.js
@@ -0,0 +1,63 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongo = require('mongodb');
+const bodyParser = require('body-parser');
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://ADMIN:Cashmoney555@blindlovecluster-k4w0u.mongodb.net/blindLoveDB?retryWrites=true&w=majority';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var eventsRouter = require('./routes/events');
+var roomsRouter = require('./routes/rooms');
+
+var app = express();
+
+const port = 5000;
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/events', eventsRouter);
+app.use('/rooms', roomsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
